a91188e8082c68a50dda0d8b81ad1bda854d3f45
Samah Barwari <sib1998@yahoo.com>
2023-09-21 19:53:45 -0500

    server side update request

--- healthpubapi/models/appointment.py
+++ healthpubapi/models/appointment.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Appointment(models.Model):
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE, related_name='submitted_appointment')
-    employee = models.ForeignKey("Employee", on_delete=models.CASCADE, related_name='assigned_employee')
+    employee = models.ForeignKey("Employee", on_delete=models.CASCADE, related_name='assigned_employee', null=True, blank=True)
     description = models.CharField(max_length=400)
     symptom_type = models.ForeignKey("SymptomType", on_delete=models.CASCADE)
     date = models.DateField()
--- healthpubapi/views/appointment_view.py
+++ healthpubapi/views/appointment_view.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from healthpubapi.models import Appointment
+from healthpubapi.models import Appointment, Customer, SymptomType
 
 
 class AppointmentView(ViewSet):
@@ -30,6 +30,50 @@ class AppointmentView(ViewSet):
         appointment = Appointment.objects.get(pk=pk)
         serialized = AppointmentSerializer(appointment, context={'request': request})
         return Response(serialized.data, status=status.HTTP_200_OK)
+    
+
+    
+    def create(self, request):
+
+
+        appointment = Appointment.objects.create(
+        customer=Customer.objects.get(user=request.auth.user),
+        description=request.data["description"],
+        symptom_type=SymptomType.objects.get(pk=request.data["symptom_type"]),
+        date=request.data["date"],
+        )
+
+        serializer = AppointmentSerializer(appointment)
+        return Response(serializer.data)
+    
+
+
+
+    def destroy(self, request, pk):
+        appointment = Appointment.objects.get(pk=pk)
+        appointment.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+
+
+    def update(self, request, pk):
+
+        appointment = Appointment.objects.get(pk=pk)
+        appointment.customer=Customer.objects.get(user=request.auth.user)
+        appointment.description = request.data["description"]
+        appointment.symptom_type=SymptomType.objects.get(pk=request.data["symptom_type"])
+        appointment.date = request.data["date"]
+
+
+        symptom_type = SymptomType.objects.get(pk=pk)
+        appointment.symptom_type = symptom_type
+        appointment.save()
+
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+        
+
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
